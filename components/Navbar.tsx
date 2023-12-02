1cc8759c2ef2ef54595fefcd1b43ea3a1ac10268
lee juyoung <juyung0903@naver.com>
2023-12-02 18:14:24 +0900

    feat: 내비게이션 바 디자인

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import DarkModeButton from './DarkModeButton';
+import SideBarButton from './SideBarButton';
+import Link from 'next/link';
 const Navbar: React.FC = () => {
+  const [darkmode, setDarkMode] = useState(false);
+  const [sidebar, setSideBar] = useState(false);
+  
+  console.log(sidebar);
   return (
-    <header>
-      <div className='font-bold'>Phnml1{`'`}s blog</div>
+    <header className="w-full flex justify-center">
+      <div className="flex w-3/4 h-20 justify-between items-center">
+        <div className="flex gap-20 items-center ml-3">
+          <Link href = '/'><div className="font-bold font-kanit text-2xl">Phnml1</div></Link>
+          <div className='text-lg'>이주영의 개발 기록장</div>
+        </div>
+        <div className='mr-3 flex gap-2'>
+          <DarkModeButton setDarkMode = {setDarkMode} mode = {darkmode}/>
+          <SideBarButton setSideBar = {setSideBar}/>
+        </div>
+      </div>
     </header>
   );
 };
