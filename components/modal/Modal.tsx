904310072d7c092b9c72884bdb316e3733b2a884
lee juyoung <juyung0903@naver.com>
2023-12-03 19:56:46 +0900

    feat: 모달창 디자인

---
+++ components/modal/Modal.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ModalButton from './components/ModalButtton';
+
+const Modal:React.FC = () => {
+  return <div className='fixed flex justify-center items-start top-0 left-0 bottom-0 right-0 bg-black bg-opacity-40 z-50' style={{padding:'12vh 16px'}}>
+    <div className='w-full flex flex-col items-center max-w-2xl h-full bg-white rounded-lg shadow-xl overflow-auto'>
+      <ModalButton theme = "Category" contents = {[{korName:'웹',engName:'web',},{korName: '알고리즘', engName: 'algorhythm'},{korName: '리엑트', engName: 'React'},{korName: '자바스크립트', engName: 'javascript'}]}/>
+      <ModalButton theme = 'Page' contents = {[{korName:'메인',engName:'main',},{korName: '모든 포스트', engName: 'All Posts'}]}/>
+      <ModalButton theme = 'preference' contents = {[{korName:'다크 모드',engName:'darkmode'}]}/>
+      {/* <div className='py-4 mt-4 w-full h-3'><div className='mx-8 text-sm text-slate-800'>Page</div></div>
+      <div className='mt-4 w-full h-8 py-4'><div className='mx-12 text-md text-black'>메인 페이지 (Main)</div></div>
+      <div className='mt-4 w-full h-8 py-4'><div className='mx-12 text-md text-black'>블로그 페이지 (Blog)</div></div> */}
+      <div className='py-4 mt-8 w-full h-3'><div className='mx-8 text-sm text-slate-800'>About me</div></div>
+      <div className='mt-4 w-full flex flex-col items-center'>
+      <div className='w-1/2 text-center mt-8'>프론트엔드 개발을 공부하고 있는<br/> 이주영 이라고 합니다.</div>
+      <div className='mt-4 w-full flex flex-col items-center'>
+      <div className='w-1/2 mt-2 text-center text-sm text-gray-400'>한국공학대학교 3학년</div>
+      <div className='w-1/2 mt-2 mb-4 text-center text-sm text-gray-400'>juyung0903@gmail.com</div>
+      </div>
+      </div>
+    </div>
+  </div>
+}
+export default Modal;
---
+++ components/modal/components/ModalButtton.tsx
@@ -0,0 +1,20 @@
+interface ModalButtonProps {
+  theme: string; // 혹은 실제로 사용되는 테마 값에 맞게 수정
+  contents: {
+    korName: string;
+    engName: string;
+  }[];
+}
+
+const ModalButton: React.FC<ModalButtonProps> = (props:ModalButtonProps) => {
+  return ( 
+  <div className='w-full'>
+      <div className='py-4 mt-4 mb-4  w-full h-3'><div className='mx-8 text-sm text-slate-800'>{props.theme}</div></div>
+      {
+        props.contents.map((a,i) => (
+      <div key = {i} className='w-full h-auto py-4 cursor-pointer rounded-lg hover:bg-slate-200'><div className='mx-12 text-md text-black'>{a.korName}({a.engName})</div></div>
+        ))
+      }
+      </div>);
+}
+export default ModalButton;
