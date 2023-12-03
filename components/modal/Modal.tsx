4871d9d9134f19571d09d673c32a95b52bf4469b
lee juyoung <juyung0903@naver.com>
2023-12-04 04:15:54 +0900

    fix: 모달창 스크롤바 때문에 border-radius적용 안되던 문제해결

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -3,7 +3,8 @@ import ModalButton from './components/ModalButtton';
 
 const Modal:React.FC = () => {
   return <div className='fixed flex justify-center items-start top-0 left-0 bottom-0 right-0 bg-black bg-opacity-40 z-50' style={{padding:'12vh 16px'}}>
-    <div className='w-full flex flex-col items-center max-w-2xl h-full bg-white rounded-lg shadow-xl overflow-auto'>
+    <div className='w-full flex flex-col items-center justify-center max-w-2xl h-full bg-white rounded-lg shadow-xl'>
+      <div className='w-full h-full my-2 flex flex-col items-start overflow-auto'>
       <ModalButton theme = "Category" contents = {[{korName:'웹',engName:'web',},{korName: '알고리즘', engName: 'algorhythm'},{korName: '리엑트', engName: 'React'},{korName: '자바스크립트', engName: 'javascript'}]}/>
       <ModalButton theme = 'Page' contents = {[{korName:'메인',engName:'main',},{korName: '모든 포스트', engName: 'All Posts'}]}/>
       <ModalButton theme = 'preference' contents = {[{korName:'다크 모드',engName:'darkmode'}]}/>
@@ -19,6 +20,7 @@ const Modal:React.FC = () => {
       </div>
       </div>
     </div>
+      </div>
   </div>
 }
 export default Modal;
