ad95df07fd0a1ee3cec016930da8c20f9272c1c9
lee juyoung <juyung0903@naver.com>
2023-12-09 22:55:02 +0900

    feat: 모달창에 link 연결

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -26,9 +26,9 @@ const Modal:React.FC<ModalProps> = (props:ModalProps) => {
   return <div className='fixed z-40 flex justify-center items-start top-0 left-0 bottom-0 right-0 bg-black bg-opacity-40' onClick={handleOutsideClick}  style={{padding:'12vh 16px'}}>
     <div ref={modalRef} className='relative w-full flex flex-col items-center justify-center max-w-2xl h-full bg-white rounded-lg shadow-xl transition-all z-50 transform-gpu'>
       <div className='w-full h-full my-2 flex flex-col items-start overflow-auto'>
-      <ModalButton theme = "Category" contents = {[{korName:'웹',engName:'web',},{korName: '알고리즘', engName: 'algorhythm'},{korName: '리엑트', engName: 'React'},{korName: '자바스크립트', engName: 'javascript'}]}/>
-      <ModalButton theme = 'Page' contents = {[{korName:'메인',engName:'main',},{korName: '모든 포스트', engName: 'All Posts'}]}/>
-      <ModalButton theme = 'preference' contents = {[{korName:'다크 모드',engName:'darkmode'}]}/>
+      <ModalButton theme = "Category" setModal = {props.setModal} contents = {[{korName:'웹',engName:'web',},{korName: '알고리즘', engName: 'algorhythm'},{korName: '리엑트', engName: 'React'},{korName: '자바스크립트', engName: 'javascript'}]}/>
+      <ModalButton theme = 'Page' setModal = {props.setModal} contents = {[{korName:'메인',engName:'main',},{korName: '모든 포스트', engName: 'All Posts'}]}/>
+      <ModalButton theme = 'preference' setModal = {props.setModal} contents = {[{korName:'다크 모드',engName:'darkmode'}]}/>
       <div className='py-4 mt-8 w-full h-3'><div className='mx-8 text-sm text-slate-800'>About me</div></div>
       <div className='mt-4 w-full flex flex-col items-center'>
       <div className='w-1/2 text-center mt-8'>프론트엔드 개발을 공부하고 있는<br/> 이주영 이라고 합니다.</div>
