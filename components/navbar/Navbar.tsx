73048dac4df804631ab0c6546f12305e07c6d8b4
lee juyoung <juyung0903@naver.com>
2023-12-27 00:31:48 +0900

    fix: postContent도 title받도록, Navbar에서 블로그 페이지로의 링크 추가

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = () => {
       <div className="flex w-full md:w-4/5 h-20 justify-between items-center">
         <div className="flex gap-24 items-center ml-8">
           <Link href = '/'><div className="font-bold font-kanit text-2xl">Phnml1</div></Link>
-          <div className='text-lg hidden md:block'>이주영의 개발 블로그</div>
+          <Link href = '/posts' className='text-lg hidden md:block'>이주영의 개발 블로그</Link>
         </div>
         <div className='mr-8 flex gap-2'>
           <DarkModeButton setDarkMode = {setDarkMode} mode = {darkmode}/>
--- pages/[...detail]/index.tsx
+++ pages/[...detail]/index.tsx
@@ -1,15 +1,13 @@
-import TOC from '@/components/detail/Toc';
 import PostContent from '@/components/detail/post-content';
 import PostHeader from '@/components/detail/post-header';
 import { getAllPosts, getPostData } from '@/utils/Post-Util';
 
 export function PostDetailPage(props) {
-  
   return (
     <div className="mt-4 w-full md:w-4/5 px-8 flex flex-col items-center">
       <PostHeader title={props.post.title} category={props.category} date={props.post.date} tag={props.post.tag}/>
       <div className='w-full flex mt-8'>
-        <PostContent content={props.post.content} slug = {props.post.slug}/>
+        <PostContent title = {props.post.title} content={props.post.content} slug = {props.post.slug}/>
       </div>
     </div>
   );
