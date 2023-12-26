f257a51be211238c43b81b7053f2183045b45d2d
lee juyoung <juyung0903@naver.com>
2023-12-27 00:29:52 +0900

    fix: 영어 이름의 heading 일시, 인식 못하는 것 수정

--- components/detail/Toc.tsx
+++ components/detail/Toc.tsx
@@ -6,7 +6,7 @@ interface PostContentProps {
   title: string;
 }
 
-const TOC = (props) => {
+const TOC:React.FC<PostContentProps> = (props) => {
   const [currentId, setCurrentId] = useState<string>('');
   const [headingEls, setHeadingEls] = useState<HTMLElement[]>([]);
   useEffect(() => {
@@ -16,20 +16,21 @@ const TOC = (props) => {
   useObservation(setCurrentId, headingEls);
   return (
     <div className="mt-12 ml-auto relative">
-      <div className='sticky top-32 min-w-[240px]'>
+      <div className='sticky top-32 w-60'>
       
         <div className='p-4  rounded-xl border-solid border-slate-300 border-2 items-start gap-6'>
-        <div className='font-bold mb-2'>Table of Content</div>
+        <div className='font-bold mb-2'>{props.title}</div>
         <ul className='mb-2 text-sm flex flex-col gap-2'>
           {
             headingEls.map((a,i) => {
               const elementType:string = a.nodeName;
               const elementContent:string = a.innerHTML;
-              const isCurrent:string = currentId==a.innerHTML? 'font-bold':'';
+              const tocId:string = a.id;
+              const isCurrent:string = currentId==tocId? 'font-bold':'';
               if(elementType == 'H2') { 
-                return (<li key={i} className={`${isCurrent}`}><Link href={`#${elementContent}`} onClick={() => {setCurrentId(elementContent)}} className='py-1'>{elementContent}</Link></li>)
+                return (<li key={i} ><Link href={`#${tocId}`} onClick={() => {setCurrentId(tocId)}} className={`${isCurrent} py-1`}>{elementContent}</Link></li>)
               } else {
-                return (<li key={i} className={`${isCurrent} ml-4`}><Link href={`#${elementContent}`} onClick={() => {setCurrentId(elementContent)}}>{elementContent}</Link></li>)
+                return (<li key={i} className={`${isCurrent} ml-4`}><Link href={`#${tocId}`} onClick={() => {setCurrentId(tocId)}} className={`${isCurrent} py-1`}>{elementContent}</Link></li>)
               }
             }
             )}
