4685a2e39c19d075afa3ef40d8d41263b8dedce5
lee juyoung <juyung0903@naver.com>
2023-12-30 16:39:56 +0900

    fix: 사용자가 지정한 테마가 아닌 systemTheme에따라 강제로 색이 변경되는 문제해결

--- components/navbar/components/DarkModeButton.tsx
+++ components/navbar/components/DarkModeButton.tsx
@@ -1,21 +1,24 @@
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-const DarkModeButton : React.FC  = () => {
-  const { theme, setTheme, systemTheme } = useTheme();
-  console.log(systemTheme);
-  useEffect(()=> {
-    setTheme(systemTheme);
-  },[systemTheme])
+const DarkModeButton: React.FC = () => {
+  const { setTheme, resolvedTheme } = useTheme();
+
   return (
-    <div className = 'cursor-pointer transition-all w-auto h-auto p-1 rounded-lg hover:bg-slate-200 dark:hover:bg-gray-700' onClick={()=>{
-      setTheme(theme === 'dark' ? 'light' : 'dark');
-  }}>
-      {theme=='dark' && (<Image src = '/navbar/night-mode.png' width={40} height={40} alt = 'switch to daymode'/>)}
-      {theme=='light' && (<Image src = '/navbar/day-mode.png' width={40} height={40} alt = 'switch to darkmode'/>)}
-      
+    <div
+      className="cursor-pointer transition-all w-auto h-auto p-1 rounded-lg hover:bg-slate-200 dark:hover:bg-gray-700"
+      onClick={() => {
+        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+      }}
+    >
+      {resolvedTheme == 'dark' && (
+        <Image src="/navbar/night-mode.png" width={40} height={40} alt="switch to daymode" />
+      )}
+      {resolvedTheme == 'light' && (
+        <Image src="/navbar/day-mode.png" width={40} height={40} alt="switch to darkmode" />
+      )}
     </div>
-  )
-}
+  );
+};
 export default DarkModeButton;
