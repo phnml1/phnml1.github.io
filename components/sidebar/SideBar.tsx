672e7e3cd2dd9b61479b508918dae8359a7ecae4
lee juyoung <juyung0903@naver.com>
2024-01-10 15:06:45 +0900

    feat: 모달창 카테고리 불러오기 및 다크모드 스타일 적용

--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import SideBarLinks from './components/SideBarLinks';
-import { getCategory } from '@/utils/Post-Util';
+import { allCategorys } from '@/utils/Post-Util';
 
 interface SidebarProps {
   setSideBar: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,7 +8,6 @@ interface SidebarProps {
 
 const SideBar: React.FC<SidebarProps> = (props: SidebarProps) => {
   const sideBarRef = useRef<HTMLDivElement | null>(null);
-  const [category, setCategory] = useState();
 
 
   const handleOutsideClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
@@ -32,42 +31,37 @@ const SideBar: React.FC<SidebarProps> = (props: SidebarProps) => {
     >
       <div
         ref={sideBarRef}
-        className="relative w-full flex flex-col items-center justify-center max-w-2xl h-full bg-white rounded-lg shadow-xl transition-all z-50 transform-gpu"
+        className="relative w-full flex flex-col items-center justify-center max-w-2xl h-full bg-white bg-rounded-lg shadow-xl transition-all z-50 transform-gpu dark:bg-dark-primary"
       >
         <div className="w-full h-full my-2 flex flex-col items-start overflow-auto">
           <SideBarLinks
             theme="Category"
             setSideBar={props.setSideBar}
-            contents={[
-              { korName: '웹', engName: 'web' },
-              { korName: '알고리즘', engName: 'algorhythm' },
-              { korName: '리엑트', engName: 'React' },
-              { korName: '자바스크립트', engName: 'javascript' },
-            ]}
+            contents={allCategorys}
           />
           <SideBarLinks
             theme="Page"
             setSideBar={props.setSideBar}
             contents={[
-              { korName: '메인', engName: 'main' },
-              { korName: '모든 포스트', engName: 'All Posts' },
+              '메인',
+              '모든 포스트'
             ]}
           />
           <SideBarLinks
             theme="preference"
             setSideBar={props.setSideBar}
-            contents={[{ korName: '다크 모드', engName: 'darkmode' }]}
+            contents={['다크 모드']}
           />
           <div className="py-4 mt-8 w-full h-3">
-            <div className="mx-8 text-sm text-slate-800">About me</div>
+            <div className="mx-8 text-sm text-slate-800 dark:text-dark-primary">About me</div>
           </div>
           <div className="mt-4 w-full flex flex-col items-center">
             <div className="w-1/2 text-center mt-8">
               프론트엔드 개발을 공부하고 있는
               <br /> 이주영 이라고 합니다.
             </div>
-            <div className="mt-4 w-full flex flex-col items-center">
-              <div className="w-1/2 mt-2 text-center text-sm text-gray-400">
+            <div className="mt-4 w-full flex flex-col items-center text-gray-400 dark:text-dark-primary">
+              <div className="w-1/2 mt-2 text-center text-sm ">
                 한국공학대학교 3학년
               </div>
               <div className="w-1/2 mt-2 mb-4 text-center text-sm text-gray-400">
--- components/sidebar/components/SideBarLinks.tsx
+++ components/sidebar/components/SideBarLinks.tsx
@@ -1,30 +1,57 @@
+import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface SideBarLinksProps {
   theme: string; // 혹은 실제로 사용되는 테마 값에 맞게 수정
-  contents: {
-    korName: string;
-    engName: string;
-  }[];
+  contents: string[];
   setSideBar: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const PageLinksObj = {
-  'All Posts': 'posts',
-  'main': '',
-  'web': 'posts/web',
-  'algorhythm': 'posts/algorhythm',
-  'React': 'posts/React',
-  'javascript': 'posts/javascript',
+  '모든 포스트': '/posts',
+  '홈': '/',
 }
-const SideBarLinks: React.FC<SideBarLinksProps> = (props:SideBarLinksProps) => {
 
+const SideBarLinks: React.FC<SideBarLinksProps> = (props:SideBarLinksProps) => {
+  const { systemTheme, setTheme, resolvedTheme } = useTheme();
+  const [prevsys, setPrevsys] = useState(systemTheme);
+  useEffect(() => {
+    if (prevsys != systemTheme) {
+      setTheme(systemTheme);
+      setPrevsys(systemTheme);
+    }
+  }, [systemTheme]);
   return ( 
   <div className='w-full'>
-      <div className='py-4 mt-4 mb-4  w-full h-3'><div className='mx-8 text-sm text-slate-800'>{props.theme}</div></div>
+      <div className='py-4 mt-4 mb-4  w-full h-3'><div className='mx-8 text-sm text-slate-800 dark:text-gray-400'>{props.theme}</div></div>
       {
-        props.contents.map((a,i) => (
-      <Link key = {i} href={`/${PageLinksObj[a.engName]}`} onClick={()=>{props.setSideBar(false)}}><div  className='w-full h-auto py-4 cursor-pointer rounded-lg hover:bg-slate-200'><div className='mx-12 text-md text-black'>{a.korName}({a.engName})</div></div></Link>
-        ))
+        props.contents.map((a, i) => {
+          if (props.theme === 'Page') {
+            return (
+              <Link key={i} href={`${PageLinksObj[a]}`} onClick={() => { props.setSideBar(false) }}>
+                <div className='w-full h-auto py-4 cursor-pointer rounded-lg hover:bg-slate-200 '>
+                  <div className='mx-12 text-md text-black dark:text-dark-primary'>{a}</div>
+                </div>
+              </Link>
+            );
+          }
+          if (props.theme == 'preference') {
+            return (<div key={i} onClick={() => { setTheme(resolvedTheme === 'dark' ? 'light' : 'dark'); }}>
+            <div className='w-full h-auto py-4 cursor-pointer rounded-lg hover:bg-slate-200'>
+              <div className='mx-12 text-md text-black dark:text-dark-primary'>{(resolvedTheme == 'dark')? '라이트 모드': '다크 모드'}</div>
+            </div>
+          </div>)
+          }
+          else {
+            return (
+              <Link key={i} href={`/posts/${a}`} onClick={() => { props.setSideBar(false) }}>
+                <div className='w-full h-auto py-4 cursor-pointer rounded-lg hover:bg-slate-200'>
+                  <div className='mx-12 text-md text-black dark:text-dark-primary'>{a}</div>
+                </div>
+              </Link>
+            );
+          }
+        })
       }
       </div>);
 }
