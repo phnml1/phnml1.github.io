f6af57e35bf47561027a19b037e556ba1d18e6f6
lee juyoung <juyung0903@naver.com>
2024-01-25 03:27:03 +0900

    feat: detail page giscus적용

---
+++ components/detail/Giscus.tsx
@@ -0,0 +1,45 @@
+import { useTheme } from 'next-themes';
+import router from 'next/router';
+import { useEffect, useRef } from 'react';
+
+export default function Giscus() {
+  const ref = useRef<HTMLDivElement>(null);
+  const { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === 'dark' ? 'dark' : 'light';
+
+  useEffect(() => {
+    if (!ref.current || ref.current.hasChildNodes()) return;
+
+    const scriptElem = document.createElement('script');
+    scriptElem.src = 'https://giscus.app/client.js';
+    scriptElem.async = true;
+    scriptElem.crossOrigin = 'anonymous';
+
+    scriptElem.setAttribute('data-repo', 'phnml1/my-blog');
+    scriptElem.setAttribute('data-repo-id', 'R_kgDOKznePw');
+    scriptElem.setAttribute('data-category', 'General');
+    scriptElem.setAttribute('data-category-id', 'DIC_kwDOKzneP84CcqiL');
+    scriptElem.setAttribute('data-mapping', 'pathname');
+    scriptElem.setAttribute('data-strict', '0');
+    scriptElem.setAttribute('data-reactions-enabled', '1');
+    scriptElem.setAttribute('data-emit-metadata', '0');
+    scriptElem.setAttribute('data-input-position', 'bottom');
+    scriptElem.setAttribute('data-theme', theme);
+    scriptElem.setAttribute('data-lang', 'en');
+
+    ref.current.appendChild(scriptElem);
+  }, []);
+
+  useEffect(() => {
+    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+    iframe?.contentWindow?.postMessage({ giscus: { setConfig: { theme } } }, 'https://giscus.app');
+  }, [theme]);
+
+  // https://github.com/giscus/giscus/blob/main/ADVANCED-USAGE.md#isetconfigmessage
+  useEffect(() => {
+    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+    iframe?.contentWindow?.postMessage({ giscus: { setConfig: { term: router.asPath } } }, 'https://giscus.app');
+  }, [router.asPath]);
+
+  return <section ref={ref} />;
+}
--- components/detail/PostFooter.tsx
+++ components/detail/PostFooter.tsx
@@ -1,5 +1,6 @@
 import MyProFile from "../MyProFile";
 import DetailNav from "./DetailNav";
+import Giscus from "./Giscus";
 
 interface PostFooterProps {
   prevData: {title:string, summary:string, slug:string} | number;
@@ -8,8 +9,8 @@ interface PostFooterProps {
 const PostFooter:React.FC <PostFooterProps> = (props) => {
   return (<div className="w-full mt-12">
     <MyProFile />
-    
     <DetailNav prevData = {props.prevData} nextData={props.nextData}/>
+    <Giscus />
   </div>);
 }
 export default PostFooter;
