7f62824889f63779009755ce83b8702a75fd2ac6
lee juyoung <juyung0903@naver.com>
2024-01-31 17:29:55 +0900

    feat: Navbar 스크롤시 사라지게

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,16 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { SetStateAction, useCallback, useEffect, useState } from 'react';
 import DarkModeButton from './components/DarkModeButton';
 import SideBarButton from './components/SideBarButton';
 import Link from 'next/link';
 import SideBar from '../sidebar/SideBar';
 import Line from '../Line';
+
 const Navbar: React.FC = () => {
   const [sidebar, setSideBar] = useState(false);
-  const [scrollHeight, setScrollHeight] = useState('h-20');
+  const [scrollHeight, setScrollHeight] = useState('translate-y-0');
+  const [position, setPosition] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [position]);
+
+  const handleScroll = useCallback(() => {
+    const moving = window.scrollY;
+    setVisible(position > moving);
+    setPosition(moving);
+  }, [position]);
+  useEffect(() => {
+    if(visible) {
+      setScrollHeight('translate-y-0');
+    } else {
+      setScrollHeight('-translate-y-full');
+    }
+  },[visible])
   return (
-    <header className="w-full sticky top-0 z-40 backdrop-blur-lg">
+    <header className={`w-full sticky top-0 z-40 ${scrollHeight} transition-all backdrop-blur-lg`}>
       <div className='w-full flex justify-center '>
-      <div className={`flex w-full md:w-4/5 ${scrollHeight} transition-all justify-between items-center`}>
+      <div className={`flex w-full md:w-4/5  h-20  justify-between items-center`}>
         <div className="flex gap-24 items-center ml-8">
           <Link href = '/'><div className="font-bold font-kanit text-2xl">Phnml1</div></Link>
           <Link href = '/posts/all' className='text-lg hidden md:block'>이주영의 개발 블로그</Link>
