1452568f11d15f33d5a3c4a5ef99a152adb14f8a
lee juyoung <juyung0903@naver.com>
2024-04-12 15:29:00 +0900

    feat: 블로그 소갯말 컴포넌트 추가

---
+++ components/posts/BlogIntroduce.tsx
@@ -0,0 +1,11 @@
+const BlogIntroduce = () => {
+  return (<div className="w-full flex flex-col items-center px-8 mt-6">
+    <div className="w-full md:w-4/5 text-3xl md:text-5xl font-extrabold ml-4 mb-4">
+      Welcome to My Blog!👋
+    </div>
+    <div className = 'w-full md:w-4/5 ml-4 mt-4'>
+      제 블로그에 와주셔서 감사합니다!<br/>이곳에서는 제가 개발하면서 공부한 것들이나 그 이외에 다양한 카테고리의 글들이 올라올 예정입니다.
+    </div>
+  </div>)
+}
+export default BlogIntroduce;
